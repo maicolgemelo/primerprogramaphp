7de2b428207e14c32a4649534de0b904a029447f
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-11-17 12:53:10 -0500

    se agrega hoja de estilo css

---
+++ biografia_connor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <title>biografia connor</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css"
+</head>
+<body>
+    <h1>biografia de connor</h1>
+    <p>Ratonhnhaké:ton (1756 - ¿?), conocido mayormente como Connor Kenway, 
+        fue un Maestro Asesino Kanien'kehá:ka durante el período de la Guerra de la Revolución Americana, capitán de la Aquila, 
+        heredero de la Hacienda Davenport y líder de la Hermandad de los Asesinos Coloniales. Nació del Templario Haytham Kenway y Kaniehtí:io,
+         una mujer Nativo Americana de la villa Kanien'kehá:ka, en Kanatahséton.</p>
+
+            <p>A una edad muy temprana cerca de sus 4 años de edad fue atacado por un grupo de Templarios cuyo objetivo era encontrar el lugar dejado por los Precursores,
+                    luego de ser noqueado se despertó para ver su villa quemándose y a su madre que lamentablemente muere en medio del incendio.
+            </p>
+
+            <img id="imagen2" src="connor1.gif">
+            <a href="index.php">volver</a>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    Hola Mundo !
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css"
+</head>
+<body>
+    <!-- Esto es un comentario-->
+    <h1>Hola mundo , me llamo Michael!</h1>
+    <h2>Subtitulo</h2>
+    <h3>contenido de informacion</h3>
+    <h4>texto pequeño</h4>
+    <h5>un texto mas pequeño</h5>
+    <h6>mas pequeño</h6>
+    <p>Esto es un parrafo colocar mucha informacion</p>
+    <p>tengo un text con estilo <b>negrita</b></p>
+    <a href="http://www.google.com" target="_blank">click aqui para ir a google</a>
+    <br>
+    hola mundo 2
+    <br>
+    Este es otro texto
+    <br>
+   <a href="biografia_connor.php"><img id="imagen1" src="connor.jpg"></a>
+</body>
+</html>
